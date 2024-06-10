aebe02492b140b3623768691999ba57b6d71fd2c
yuxunn <teeyuxun@gmail.com>
2024-06-10 11:02:31 +0800

    toggle sidebar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,8 +1,8 @@
-// src/components/Navbar.js
 import React, { useContext } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../components/firebase'; 
 import { SearchContext } from '../components/searchContext';
+import { FaSearch } from 'react-icons/fa';
 
 const Navbar = () => {
   const { searchTerm, setSearchTerm } = useContext(SearchContext);
@@ -20,9 +20,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 bg-pink-100 shadow-md">
       <div className="flex items-center flex-grow">
-        <svg className="w-6 h-6 text-gray-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405M21 21l-1.405-1.405M16 8A6 6 0 104 8a6 6 0 0012 0z"></path>
-        </svg>
+        <FaSearch className="w-6 h-6 text-gray-500 mr-2" />
         <input
           type="text"
           placeholder="Enter your search request..."
@@ -33,7 +31,7 @@ const Navbar = () => {
       </div>
       <button
         onClick={handleLogout}
-        className="ml-4 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
+        className="ml-4 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 focus:outline-none"
       >
         Log Out
       </button>
@@ -42,3 +40,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { FaHeart, FaBook, FaUsers, FaGlobe, FaPlus } from 'react-icons/fa';
+import { FaHeart, FaBook, FaUsers, FaGlobe, FaPlus, FaBars, FaAngleDoubleRight , FaAngleDoubleLeft} from 'react-icons/fa';
 import { VscAccount } from "react-icons/vsc";
 import strawberrycake from '../assets/strawberrycake.png';
-import { doc, getDoc } from 'firebase/firestore';
-import { auth, db } from '../components/firebase'; 
+import { doc, getDoc, collection, query, where, getDocs } from 'firebase/firestore';
+import { auth, db } from '../components/firebase';
 
 const Sidebar = () => {
   const [userDetails, setUserDetails] = useState(null);
+  const [recipeCount, setRecipeCount] = useState(0);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const fetchUserData = async (user) => {
     if (user) {
       const docRef = doc(db, "Users", user.uid);
       const docSnap = await getDoc(docRef);
-  
+
       if (docSnap.exists()) {
         setUserDetails(docSnap.data());
+
+        const recipesRef = collection(db, "recipes");
+        const q = query(recipesRef, where("userId", "==", user.uid));
+        const querySnapshot = await getDocs(q);
+
+        setRecipeCount(querySnapshot.size);
       } else {
         console.log("No such document!");
       }
@@ -30,52 +38,73 @@ const Sidebar = () => {
     return () => unsubscribe();
   }, []);
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="h-screen bg-white shadow-lg w-64 space-y-6 py-7 px-2">
-      <div className="flex items-center justify-center space-x-2">
-        <img
-          className="w-8 h-8"
-          src={strawberrycake} 
-          alt="Logo"
-        />
-        <h1 className="text-2xl font-bold">ekac</h1>
-      </div>
-      <div className="flex flex-col items-center">
-        <VscAccount className="w-24 h-16 rounded-full" alt="User Avatar" />
-        {userDetails ? (
-          <>
-            <h2 className="mt-2 text-lg font-semibold">{userDetails.username}</h2>
-            <div className="mt-4 bg-red-100 text-red-600 py-2 px-4 rounded-full flex items-center">
-              <span className="text-2xl">🍲</span> 
-              <span className="ml-2">37</span>
-            </div>
-          </>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-      <nav className="space-y-2">
-        <a href="/addNewRecipe" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
+    <div className="relative">
+      <button
+        onClick={toggleSidebar}
+        className={`fixed top-4 left-4 z-20 p-2 bg-transparent text-gray-800 rounded-full focus:outline-none ${isSidebarOpen ? 'hidden' : 'block'}`}
+      >
+        <FaAngleDoubleRight />
+      </button>
+      <div className={`fixed h-screen bg-white shadow-lg w-64 space-y-6 py-7 px-2 z-10 transition-transform duration-300 ${isSidebarOpen ? 'transform translate-x-0' : 'transform -translate-x-full'}`}>
+        <button
+          onClick={toggleSidebar}
+          className={`absolute top-4 right-4 z-20 p-2 bg-transparent text-gray-800 rounded-full focus:outline-none ${isSidebarOpen ? 'block' : 'hidden'}`}
+        >
+          <FaAngleDoubleLeft />
+        </button>
+        <div className="flex items-center justify-center space-x-2">
+          <img
+            className="w-8 h-8"
+            src={strawberrycake}
+            alt="Logo"
+          />
+          <h1 className="text-2xl font-bold">ekac</h1>
+        </div>
+        <div className="flex flex-col items-center">
+          <VscAccount className="w-24 h-16 rounded-full" alt="User Avatar" />
+          {userDetails ? (
+            <>
+              <h2 className="mt-2 text-lg font-semibold">{userDetails.username}</h2>
+              <div className="mt-4 bg-red-100 text-red-600 py-2 px-4 rounded-full flex items-center">
+                <span className="text-2xl">🍲</span>
+                <span className="ml-2">{recipeCount}</span>
+              </div>
+            </>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+        <nav className="space-y-2">
+          <a href="/addNewRecipe" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
             <FaPlus className=" mr-3" />
             <span>Add New Recipe</span>
           </a>
-        <a href="/" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
-          <FaBook className=" mr-3" />
-          <span>Recipes</span>
-        </a>
-        <a href="/favorites" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
-          <FaHeart className=" mr-3" />
-          <span>Favorites</span>
-        </a>
-        <a href="/courses" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
-          <FaUsers className="mr-3" />
-          <span>Courses</span>
-        </a>
-        <a href="/community" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
-          <FaGlobe className=" mr-3" />
-          <span>Community</span>
-        </a>
-      </nav>
+          <a href="/" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
+            <FaBook className=" mr-3" />
+            <span>Recipes</span>
+          </a>
+          <a href="/favorites" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
+            <FaHeart className=" mr-3" />
+            <span>Favorites</span>
+          </a>
+          <a href="/courses" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
+            <FaUsers className="mr-3" />
+            <span>Courses</span>
+          </a>
+          <a href="/community" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-100 flex items-center text-gray-600">
+            <FaGlobe className=" mr-3" />
+            <span>Community</span>
+          </a>
+        </nav>
+      </div>
+      <div className={`main-content transition-margin duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-0'}`}>
+        {/* Main page content goes here */}
+      </div>
     </div>
   );
 };
