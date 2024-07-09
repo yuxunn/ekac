6c1f8e45126970074e1a98a7017738ce0ac1ef6c
yuxunn <teeyuxun@gmail.com>
2024-07-09 16:29:38 +0800

    favourites page

--- frontend/src/pages/favourites.js
+++ frontend/src/pages/favourites.js
@@ -1,10 +1,8 @@
-// src/pages/Home.js
 import React, { useEffect, useState, useContext } from 'react';
 import { collection, query, onSnapshot } from 'firebase/firestore';
 import { auth, db } from '../components/firebase'; 
 import CardFav from '../components/cardfav';
 import { SearchContext } from '../components/searchContext';
-
 
 const Favourites = () => {
   const [recipes, setRecipes] = useState([]);
@@ -40,9 +38,15 @@ const Favourites = () => {
 
   return (
     <main className="p-4 pt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {filteredRecipes.map((recipe) => (
-        <CardFav key={recipe.id} isFavorited={true} {...recipe} />
-      ))}
+      {filteredRecipes.length > 0 ? (
+        filteredRecipes.map((recipe) => (
+          <CardFav key={recipe.id} isFavorited={true} {...recipe} />
+        ))
+      ) : (
+        <div className="col-span-full text-center text-gray-600">
+        You have no favourited recipes yet! 😭 Start to ❤️ the recipes you like now!
+        </div>
+      )}
     </main>
   );
 };
