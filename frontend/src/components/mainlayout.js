0aacacd2c5459b192eabb474b2e89c89d83c777c
yuxunn <teeyuxun@gmail.com>
2024-07-09 15:51:47 +0800

    refactor

--- frontend/src/components/mainlayout.js
+++ frontend/src/components/mainlayout.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './navbar';
 import Sidebar from './sidebar';
+import SearchBar from './searchBar';
+import { FaFilter } from "react-icons/fa";
 
 const MainLayout = ({ children }) => {
+  const [filter, setFilter] = useState('None');
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex-1 bg-pink-100 min-h-screen">
         <Navbar />
-        {children}
+        <div className="flex justify-between items-center p-4">
+          <div className="flex items-center">
+          <FaFilter  />
+            <label htmlFor="filter" className="mr-2 ml-2 font-semibold">Filter by:</label>
+            <select
+              id="filter"
+              value={filter}
+              onChange={handleFilterChange}
+              className="border border-gray-300 rounded p-2 shadow-sm"
+            >
+              <option value="None">None</option>
+              <option value="Option1">Option 1</option>
+              <option value="Option2">Option 2</option>
+              <option value="Option3">Option 3</option>
+            </select>
+          </div>
+          <SearchBar />
+
+        </div>
+        <div className="-mt-8">{children}</div>
       </div>
     </div>
   );
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -23,9 +23,9 @@ const Navbar = ({ isSidebarOpen }) => {
   const getPageTitle = () => {
     switch (location.pathname) {
       case '/home':
-        return 'Recipes';
+        return 'recipes';
       case '/recipes':
-        return 'Recipes';
+        return 'recipes';
       case '/addNewRecipe':
         return 'Add New Recipe';
       case '/favourites':
@@ -41,26 +41,16 @@ const Navbar = ({ isSidebarOpen }) => {
 
   return (
     <div className="flex items-center justify-between p-4 bg-pink-100 shadow-md">
-      <div className="flex items-center space-x-4">
+<div className="flex items-center space-x-2 md:space-x-4">
         <div className={`flex items-center transition-margin duration-300 ${isSidebarOpen ? 'ml-8' : 'ml-0'}`}>
           <img className="w-6 h-6 text-gray-500 mr-2" src={strawberrycake} alt="Logo"/>
-          <h3 className="text-xl font-bold text-black">E.KAC -</h3>
+          <span className="text-xl font-bold text-black whitespace-nowrap">ekac -</span>
           <h3 className="text-xl font-bold text-black ml-1">{getPageTitle()}</h3>
         </div>
       </div>
-      {/* <div className={`flex flex-grow items-center transition-margin duration-300 ${isSidebarOpen ? 'ml-8' : 'ml-0'}`}>
-        <FaSearch className="w-6 h-6 text-gray-500 mr-2" />
-        <input
-          type="text"
-          placeholder="Enter your search request..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full p-2 bg-transparent border-b border-gray-300 placeholder-gray-500 focus:outline-none"
-        />
-      </div> */}
       <button
         onClick={handleLogout}
-        className="ml-4 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 focus:outline-none"
+        className="ml-4 px-4 py-2 bg-black text-white rounded-md hover:bg-gray focus:outline-none"
       >
         Log Out
       </button>
