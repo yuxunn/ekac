a01ad3fb9fc4839729a3def2d01380d0aa81de0e
yuxunn <teeyuxun@gmail.com>
2024-07-09 14:29:16 +0800

    change sidebar and navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,11 +3,13 @@ import { signOut } from 'firebase/auth';
 import { auth } from '../components/firebase'; 
 import { SearchContext } from '../components/searchContext';
 import { FaSearch } from 'react-icons/fa';
+import { useLocation } from 'react-router-dom';
+import strawberrycake from "../assets/strawberrycake.png";
 
-const Navbar = (isSidebarOpen) => {
+const Navbar = ({ isSidebarOpen }) => {
   const { searchTerm, setSearchTerm } = useContext(SearchContext);
-  // console.log(isSidebarOpen)
-  // console.log("hei")
+  const location = useLocation();
+
   const handleLogout = async () => {
     try {
       await signOut(auth);
@@ -18,18 +20,42 @@ const Navbar = (isSidebarOpen) => {
     }
   };
 
+  const getPageTitle = () => {
+    switch (location.pathname) {
+      case '/recipes':
+        return 'Recipes';
+      case '/addNewRecipe':
+        return 'Add New Recipe';
+      case '/favourites':
+        return 'My Favourites';
+      case '/courses':
+        return 'Courses';
+      case '/community':
+        return 'Community';
+      default:
+        return '';
+    }
+  };
+
   return (
     <div className="flex items-center justify-between p-4 bg-pink-100 shadow-md">
-    <div className={`flex flex-grow items-center transition-margin duration-300 ${isSidebarOpen ? 'ml-8' : 'ml-0'}`}>
-      <FaSearch className="w-6 h-6 text-gray-500 mr-2" />
-      <input
-        type="text"
-        placeholder="Enter your search request..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="w-full p-2 bg-transparent border-b border-gray-300 placeholder-gray-500 focus:outline-none"
-      />
-    </div>
+      <div className="flex items-center space-x-4">
+        <div className={`flex items-center transition-margin duration-300 ${isSidebarOpen ? 'ml-8' : 'ml-0'}`}>
+          <img className="w-6 h-6 text-gray-500 mr-2" src={strawberrycake} alt="Logo"/>
+          <h3 className="text-xl font-bold text-black">E.KAC -</h3>
+          <h3 className="text-xl font-bold text-black ml-1">{getPageTitle()}</h3>
+        </div>
+      </div>
+      {/* <div className={`flex flex-grow items-center transition-margin duration-300 ${isSidebarOpen ? 'ml-8' : 'ml-0'}`}>
+        <FaSearch className="w-6 h-6 text-gray-500 mr-2" />
+        <input
+          type="text"
+          placeholder="Enter your search request..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full p-2 bg-transparent border-b border-gray-300 placeholder-gray-500 focus:outline-none"
+        />
+      </div> */}
       <button
         onClick={handleLogout}
         className="ml-4 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 focus:outline-none"
@@ -41,4 +67,3 @@ const Navbar = (isSidebarOpen) => {
 };
 
 export default Navbar;
-
--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -6,7 +6,6 @@ import {
   FaGlobe,
   FaPlus,
   FaBars,
-  FaAngleDoubleRight,
   FaAngleDoubleLeft,
   FaUserCircle,
 } from "react-icons/fa";
@@ -16,16 +15,15 @@ import {
   getDoc,
   collection,
   query,
-  where,
   getDocs,
 } from "firebase/firestore";
 import { auth, db } from "../components/firebase";
-import { Link , useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 const Sidebar = () => {
   const [userDetails, setUserDetails] = useState(null);
   const [recipeCount, setRecipeCount] = useState(0);
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
 
   const navigate = useNavigate();
 
@@ -36,10 +34,7 @@ const Sidebar = () => {
 
       if (docSnap.exists()) {
         setUserDetails(docSnap.data());
-        console.log(docSnap);
-        console.log("erere");
         const q = query(collection(db, "users", user.uid, "recipes"));
-        console.log(q);
         const querySnapshot = await getDocs(q);
         setRecipeCount(querySnapshot.size);
       } else {
@@ -71,30 +66,15 @@ const Sidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
-  const handleHomeClick =() =>{
-    navigate('/recipes');
-    }
-
-
-  const handleProfileClick =() =>{
-    navigate('/profile');
-  }
+  const handleHomeClick = () => {
+    navigate("/recipes");
+  };
 
   return (
-    <div className="relative">
-      <button
-        onClick={toggleSidebar}
-        className={`fixed top-4 left-4 z-20 p-2 bg-transparent text-gray-800 rounded-full focus:outline-none ${
-          isSidebarOpen ? "hidden" : "block"
-        }`}
-      >
-        <FaAngleDoubleRight />
-      </button>
+    <div className="relative flex">
       <div
-        className={`fixed h-screen bg-white shadow-lg w-64 space-y-6 py-7 px-2 z-10 transition-transform duration-300 ${
-          isSidebarOpen
-            ? "transform translate-x-0"
-            : "transform -translate-x-full"
+        className={`fixed h-screen bg-white shadow-lg py-7 px-2 z-10 transition-transform duration-300 ${
+          isSidebarOpen ? "w-64" : "w-20"
         }`}
       >
         <button
@@ -103,86 +83,114 @@ const Sidebar = () => {
             isSidebarOpen ? "block" : "hidden"
           }`}
         >
-          <FaAngleDoubleLeft />
+          <FaAngleDoubleLeft className="text-lg" />
         </button>
-        <button className="items-center flex justify-center w-full"
-        onClick={handleHomeClick}>
+        <button
+          className="items-center flex justify-center w-full"
+          onClick={isSidebarOpen ? handleHomeClick : toggleSidebar}
+        >
+          
           <div className="flex items-center space-x-2">
-            <img className="w-8 h-8" src={strawberrycake} alt="Logo" />
-            <h1 className="text-2xl font-bold">ekac</h1>
+            {isSidebarOpen ? (
+              <img className="w-8 h-8" src={strawberrycake} alt="Logo" onClick={handleHomeClick} />
+            ) : (
+              <FaBars className="text-lg" />
+            )}
+            {isSidebarOpen && <h1 className="text-2xl font-bold">ekac</h1>}
           </div>
         </button>
-
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mt-4">
           <Link to="/profile">
             {userDetails && (
               <img
-                className="w-8 h-8"
+                className="w-8 h-8 rounded-full"
                 src={userDetails.avatar}
                 alt="User Avatar"
               />
-            )}{" "}
+            )}
           </Link>
           {userDetails ? (
             <>
-            <a href = "/profile">
-
-<h2 className="mt-2 text-lg font-semibold underline">
-                {userDetails.username}{" "}
-              </h2>
-              <div className="mt-4 bg-pink-100 text-red-600 py-2 px-4 rounded-full flex items-center">
-                <span className="text-2xl">🍲</span>
-                <span className="ml-2">{recipeCount}</span>
-              </div>
-              </a>
+              {isSidebarOpen && (
+                <>
+                  <Link to="/profile">
+                    <h2 className="mt-2 text-lg font-semibold underline">
+                      {userDetails.username}
+                    </h2>
+                  </Link>
+                  <div className="mt-4 bg-pink-100 text-red-600 py-2 px-4 rounded-full flex items-center">
+                    <span className="text-2xl">🍲</span>
+                    <span className="ml-2">{recipeCount}</span>
+                  </div>
+                </>
+              )}
             </>
           ) : (
             <p>Loading...</p>
           )}
         </div>
-    
-        <nav className="space-y-2">
+
+        <nav className="space-y-4 mt-6">
           <a
             href="/recipes"
-            className="block py-2.5 px-4 rounded transition duration-200 hover:bg-pink-100 flex items-center text-gray-600"
+            className="flex items-center py-2 px-4 rounded transition duration-200 hover:bg-pink-100 text-gray-600"
           >
-            <FaBook className=" mr-3" />
-            <span>Recipes</span>
+            <FaBook
+              className={`text-lg ${
+                isSidebarOpen ? "mr-3" : "mx-auto"
+              }`}
+            />
+            {isSidebarOpen && <span>Recipes</span>}
           </a>
           <a
             href="/addNewRecipe"
-            className="block py-2.5 px-4 rounded transition duration-200 hover:bg-pink-100 flex items-center text-gray-600"
+            className="flex items-center py-2 px-4 rounded transition duration-200 hover:bg-pink-100 text-gray-600"
           >
-            <FaPlus className=" mr-3" />
-            <span>Add New Recipe</span>
+            <FaPlus
+              className={`text-lg ${
+                isSidebarOpen ? "mr-3" : "mx-auto"
+              }`}
+            />
+            {isSidebarOpen && <span>Add New Recipe</span>}
           </a>
-
           <a
             href="/favourites"
-            className="block py-2.5 px-4 rounded transition duration-200 hover:bg-pink-100 flex items-center text-gray-600"
+            className="flex items-center py-2 px-4 rounded transition duration-200 hover:bg-pink-100 text-gray-600"
           >
-            <FaHeart className=" mr-3" />
-            <span>My Favourites</span>
+            <FaHeart
+              className={`text-lg ${
+                isSidebarOpen ? "mr-3" : "mx-auto"
+              }`}
+            />
+            {isSidebarOpen && <span>My Favourites</span>}
           </a>
           <a
             href="/courses"
-            className="block py-2.5 px-4 rounded transition duration-200 hover:bg-pink-100 flex items-center text-gray-600"
+            className="flex items-center py-2 px-4 rounded transition duration-200 hover:bg-pink-100 text-gray-600"
           >
-            <FaUsers className="mr-3" />
-            <span>Courses</span>
+            <FaUsers
+              className={`text-lg ${
+                isSidebarOpen ? "mr-3" : "mx-auto"
+              }`}
+            />
+            {isSidebarOpen && <span>Courses</span>}
           </a>
           <a
             href="/community"
-            className="block py-2.5 px-4 rounded transition duration-200 hover:bg-pink-100 flex items-center text-gray-600"
+            className="flex items-center py-2 px-4 rounded transition duration-200 hover:bg-pink-100 text-gray-600"
           >
-            <FaGlobe className=" mr-3" />
-            <span>Community</span>
+            <FaGlobe
+              className={`text-lg ${
+                isSidebarOpen ? "mr-3" : "mx-auto"
+              }`}
+            />
+            {isSidebarOpen && <span>Community</span>}
           </a>
         </nav>
       </div>
       <div
         className={`main-content transition-margin duration-300 ${
-          isSidebarOpen ? "ml-64" : "ml-0"
+          isSidebarOpen ? "ml-64" : "ml-20"
         }`}
       ></div>
     </div>
